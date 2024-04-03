61cc9c412f61793830311eac2b206204e73cba5a
Nebreoma <Nebreoma12@gmail.com>
2024-04-03 22:13:03 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+
+    }
+
+}
